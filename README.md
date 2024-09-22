c05001e8ae571253303b4f9fc39bc36cd57abf55
fres621 <126067139+fres621@users.noreply.github.com>
2024-09-22 09:39:13 +0100

    CSS
    CSS

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # slotter
 
-Temporarily only being used to access the assets in the `out` dir from css. Code for the generation of those is here as well
+Animated Twemoji slot machines
+
+![🎰](./out/result0.gif)
+
+## 🔗 Files of interest
+
+-   CSS for gambling on Discord by sending a 🎰 emoji: [discord-slots.theme.css](./discord-slots.theme.css)
+-   Generated GIFs: [out](./out) (`https://github.com/fres621/slotter/raw/refs/heads/main/out/result` ­`0-63` `.gif`)
+
+## ⚙️ Code snippets
+
+**CSS for Discord**
+
+```css
+@import https: //github.com/fres621/slotter/raw/refs/heads/main/discord-slots.theme.css;;
+```
---
+++ src/css.ts
@@ -0,0 +1,25 @@
+/** Generate CSS file */
+
+import { writeFileSync } from "fs";
+
+// Version should be bumped when a change that alters id->outcome takes place
+let header = `
+/**
+ * @name Slotter
+ * @author .fres.
+ * @version 1.0.1
+ * @description Gamble using slot machine emoji
+ * @source https://github.com/fres621/slotter/
+*/
+`;
+let css = header.trim() + "\n.message_d5deea {";
+const results = 64;
+for (let i = 0; i <= 999; i++) {
+    let number = i % results;
+    let ii = i.toString().padStart(3, "0");
+    let url = `https://github.com/fres621/slotter/raw/refs/heads/main/out/result${number}.gif`;
+    css += `&[data-list-item-id$="${ii}"] img[aria-label="🎰"]{content:url("${url}?i=${ii}");}`;
+}
+css += "\n}";
+
+writeFileSync("./discord-slots.theme.css", css);
